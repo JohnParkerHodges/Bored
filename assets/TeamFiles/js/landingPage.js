d04e7751d18137fffcfe8648f8763d6b983a29e6
JohnParkerHodges <phodgesfl@gmail.com>
2019-11-02 14:43:10 -0400

    added the rest

--- assets/TeamFiles/js/landingPage.js
+++ assets/TeamFiles/js/landingPage.js
@@ -1,1 +1,56 @@
 new WOW().init();
+
+$("#userSignUpButton").on('click', function() {
+    const formUserName = $("#userSignUpEmail").val();
+    const formUserPassword = $("#userSignUpPassword").val();
+
+    if (localStorage.getItem(`neverBoredApp-User-${formUserName}`)) {
+        localStorage.setItem('neverBoredApp-LoggedIn', false)
+        SignUpUserAlreadyExistsAlert();
+    } else {
+        localStorage.setItem(`neverBoredApp-User-${formUserName}`, formUserPassword);
+        localStorage.setItem('neverBoredApp-LoggedIn', true)
+        window.location.replace('./Main.html');
+    }
+});
+
+$("#userLogInButton").on('click', function() {
+    const formUserName = $("#userLogInEmail").val();
+    const formUserPassword = $("#userLogInPassword").val();
+
+    const userPassFromLocalStorage = localStorage.getItem(`neverBoredApp-User-${formUserName}`)
+
+    if (!userPassFromLocalStorage) {
+        localStorage.setItem('neverBoredApp-LoggedIn', false)
+        LogInValidationAlert();
+    } else {
+        if (userPassFromLocalStorage !== formUserPassword) {
+            localStorage.setItem('neverBoredApp-LoggedIn', false)
+            LogInValidationAlert();
+        } else if (userPassFromLocalStorage === formUserPassword) {
+            localStorage.setItem('neverBoredApp-LoggedIn', true)
+            window.location.replace('./Main.html');
+        }
+    }
+})
+
+
+function SignUpUserAlreadyExistsAlert() {
+    $("#userSignInForm").append(`
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        <strong>Holy guacamole!</strong> You already have an account. Click "Log in" to log in.
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                    </div>`)
+}
+
+function LogInValidationAlert() {
+    $("#userLogInForm").append(`
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>Holy guacamole!</strong> User name or password is incorrect.
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                        </div>`)
+}
--- assets/TeamFiles/js/uiux.js
+++ assets/TeamFiles/js/uiux.js
@@ -1,13 +1,16 @@
-// SideNav Button Initialization
-$(".button-collapse").sideNav();
-// SideNav Scrollbar Initialization
-var sideNavScrollbar = document.querySelector('.custom-scrollbar');
-//var ps = new PerfectScrollbar(sideNavScrollbar);
+$(document).ready(function() {
+    checkUserAuth();
+
+    // SideNav Button Initialization
+    $(".button-collapse").sideNav();
+    // SideNav Scrollbar Initialization
+    var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+    //var ps = new PerfectScrollbar(sideNavScrollbar);
 
 
-$("#seeCarsEvents").on('click', function() {
-    $(".eventCard").hide();
-    $("#mainBody").append(`
+    $("#seeCarsEvents").on('click', function() {
+        $(".eventCard").hide();
+        $("#mainBody").append(`
     <div class="container-fluid text-center">
     <h1>Loading...</h1>
     <!--Big blue-->
@@ -25,24 +28,36 @@ $("#seeCarsEvents").on('click', function() {
       </div>
     </div>
     </div>`)
-    getCarsEvents();
-})
+        getCarsEvents();
+    })
 
-$("#userAccount").on('click', function() {
-    $("#userFavorites").empty();
-    $("#userFavorites").append("<h4>Favorites:</h4>")
+    $("#userAccount").on('click', function() {
+        $("#userFavorites").empty();
+        $("#userFavorites").append("<h4>Favorites:</h4>")
 
-    for (let i = 0; i < localStorage.length; i++) {
+        for (let i = 0; i < localStorage.length; i++) {
 
-        const localStorageKey = localStorage.key(i);
-        if (localStorageKey.includes("boredAppEvent")) {
-            const event = JSON.parse(localStorage.getItem(localStorageKey));
-            console.log(event)
+            const localStorageKey = localStorage.key(i);
+            if (localStorageKey.includes("neverBoredAppEvent")) {
+                const event = JSON.parse(localStorage.getItem(localStorageKey));
+                console.log(event)
 
-            $("#userFavorites").append(`<p><a href="${event.url}" target="_blank">${event.title}</a></p>`);
+                $("#userFavorites").append(`<p><a href="${event.url}" target="_blank">${event.title}</a></p>`);
+            }
+
+
         }
 
-
-    }
-
+    })
 })
+
+function checkUserAuth() {
+    if (localStorage.getItem('neverBoredApp-LoggedIn') === "false") {
+        window.location.replace('./landingPage.html');
+    }
+}
+
+function userLogOut() {
+    localStorage.setItem('neverBoredApp-LoggedIn', false)
+    window.location.replace('./landingPage.html');
+}
